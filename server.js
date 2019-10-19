e62aabf7dc42c4ad0db7275f4f9df541252deae6
Damian <d.pagowski@gmail.com>
2019-10-19 23:09:17 +0700

    app reorganized. routes and auth code moved to separate modules

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-'use strict';
+'use strict'
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
 
-const app = express();
+const app = express()
+const routes = require('./routes.js')
+const auth = require('./auth.js')
+// DB Connection
+const url = process.env.DATABASE
+const dbName = 'test'
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(url)
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
+fccTesting(app) // For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+client.connect(function (err) {
+  if (err) {
+    console.log('Database error: ' + err)
+  } else {
+    const db = client.db(dbName)
+    auth(app, db)
+    routes(app, db)
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Listening on port ' + process.env.PORT)
+    })
+  }
+})
